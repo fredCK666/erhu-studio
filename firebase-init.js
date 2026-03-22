2254f7d638add919bc8b2dbb50bd3f27ec514979
fredCK666 <fred30793985@gmail.com>
2026-03-23 06:45:59 +0800

    Fix Firebase web app config

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,11 @@
 (function () {
   const firebaseConfig = {
-    apiKey: "AIzaSyClwRdoKTY35bqHP1OHAHsSBo50jWvqY98",
+    apiKey: "AIzaSyClwRdoKTY35bqHP1OHAHsSBo50jWwqy98",
     authDomain: "erhu-auth.firebaseapp.com",
     projectId: "erhu-auth",
     storageBucket: "erhu-auth.firebasestorage.app",
     messagingSenderId: "319906716187",
-    appId: "1:319906716187:web:29175cfb7a404590e0b6be",
+    appId: "1:319906716187:web:29175cfb7a404509e0b6be",
     measurementId: "G-RYNY92VQ51"
   };
 
